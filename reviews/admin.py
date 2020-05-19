6b1c3e327dde7681c4218df4241ac11c99190144
Alexander Ternikov <herr.potato@pm.me>
2020-05-19 07:06:15 +0300

    add formatted text to Review model

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.html import format_html_join
+from django.utils.safestring import mark_safe
 from .models import Doctor, Specialty, Review, Fword, ExceptionWord
 
 
@@ -9,15 +11,28 @@ class DoctorAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
     def get_specs(self, obj):
-        return "\n".join([spec.title for spec in obj.spec.all()])
+        return ", ".join([spec.title for spec in obj.spec.all()])
 
 
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ("author", "doctor", "text", "created",)
+    list_display = ("author", "doctor", "review_text", "review_formatted_text", "created",)
     list_select_related = ("author", "doctor",)
     readonly_fields = ("text", "created")
     search_fields = ("doctor", "description",)
     empty_value_display = '-пусто-'
+
+    def review_text(self, obj):
+        return format_html_join(
+            mark_safe('<br>'),
+            '{}',
+            ((line,) for line in obj.text.split('\n')),
+        )
+    def review_formatted_text(self, obj):
+        return format_html_join(
+            mark_safe('<br>'),
+            '{}',
+            ((line,) for line in obj.formatted_text.split('\n')),
+        )
 
 
 admin.site.register(Doctor, DoctorAdmin)
--- reviews/models.py
+++ reviews/models.py
@@ -1,16 +1,18 @@
+import re
+
 from django.contrib.auth import get_user_model
 from django.db import models
-
+from django.dispatch import receiver
 
 User = get_user_model()
 
 
 class Specialty(models.Model):
     title = models.CharField(max_length=200)
-    
+
     class Meta:
         ordering = ['title']
-    
+
     def __str__(self):
         return self.title
 
@@ -26,14 +28,37 @@ class Doctor(models.Model):
 
 
 class Review(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews", blank=True, null=True)
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, related_name="reviews")
-    text = models.TextField()
-    created = models.DateTimeField('creation date', auto_now_add=True, db_index=True)  
+    text = models.TextField(default='')
+    formatted_text = models.TextField()
+    created = models.DateTimeField('creation date', auto_now_add=True, db_index=True)
+
+    def format_text(self):
+        clean_repeated_puncts = re.sub(r'([.\?#,<>%~`!$:;])\1+', r'\1', self.text)
+        fix_spaces = re.sub(' +', ' ', clean_repeated_puncts)
+        formatted_text = re.sub(r'\s([?.!"](?:\s|$))', r'\1', fix_spaces)
+        # capitalize text if finded >6 uppercase chars in a row
+        if re.search(r'([A-ZА-Я]{6})', fix_spaces):
+            formatted_text = re.sub(r"(\A\w)|"+     # start of string
+                "(?<!\.\w)([\.?!] )\w|"+            # after a ?/!/. and a space, 
+                                                    # but not after an acronym
+                "\w(?:\.\w)|"+                      # start/middle of acronym
+                "(?<=\w\.)\w",                      # end of acronym
+                lambda x: x.group().upper(), 
+                fix_spaces.lower())
+        return formatted_text
+
+@receiver(models.signals.post_save, sender=Review)
+def execute_after_save(sender, instance, created, *args, **kwargs):
+    if created:
+        instance.formatted_text = instance.format_text()
+
 
 
 class Fword(models.Model):
     word = models.CharField('f-word', max_length=150)
+
 
 class ExceptionWord(models.Model):
     word = models.CharField('exception word', max_length=150)
