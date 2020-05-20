c7d27fa987bbdc97bbab078068bf5bcd77fbda62
Alexander Ternikov <herr.potato@pm.me>
2020-05-20 13:36:07 +0300

    Refactoring

--- reviews/models.py
+++ reviews/models.py
@@ -7,10 +7,10 @@ User = get_user_model()
 
 
 class Specialty(models.Model):
-    title = models.CharField('Специальность', max_length=200)
+    title = models.CharField("Специальность", max_length=200)
 
     class Meta:
-        ordering = ['title']
+        ordering = ["title"]
         verbose_name = "Специальность"
         verbose_name_plural = "Специальности"
 
@@ -19,9 +19,9 @@ class Specialty(models.Model):
 
 
 class Doctor(models.Model):
-    first_name = models.CharField('Имя', max_length=30, blank=True)
-    last_name = models.CharField('Фамилия', max_length=150, blank=True)
-    patronymic = models.CharField('Отчество', max_length=150, blank=True)
+    first_name = models.CharField("Имя", max_length=30, blank=True)
+    last_name = models.CharField("Фамилия", max_length=150, blank=True)
+    patronymic = models.CharField("Отчество", max_length=150, blank=True)
     spec = models.ManyToManyField(Specialty)
 
     class Meta:
@@ -30,42 +30,47 @@ class Doctor(models.Model):
 
     @property
     def get_full_name(self):
-        return f'{self.last_name} {self.first_name} {self.patronymic}'
+        return f"{self.last_name} {self.first_name} {self.patronymic}"
 
     @property
     def get_spec(self):
-        return ', '.join(spec.title for spec in self.spec.all())
+        return ", ".join(spec.title for spec in self.spec.all())
 
     def __str__(self):
-        return f'{self.last_name} {self.first_name} {self.patronymic}'
+        return f"{self.last_name} {self.first_name} {self.patronymic}"
 
 
 class Review(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews", blank=True, null=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="reviews", blank=True, null=True
+    )
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, related_name="reviews")
     ip_address = models.GenericIPAddressField()
-    text = models.TextField('Текст отзыва', default='')
-    formatted_text = models.TextField('Форматированный текст',)
-    dt_created = models.DateTimeField('Дата создания', auto_now_add=True, db_index=True)
-    dt_updated = models.DateTimeField('Дата последнего обновления', auto_now=True)
+    text = models.TextField("Текст отзыва", default="")
+    formatted_text = models.TextField("Форматированный текст",)
+    dt_created = models.DateTimeField("Дата создания", auto_now_add=True)
+    dt_updated = models.DateTimeField("Дата последнего обновления", auto_now=True)
 
     class Meta:
         verbose_name = "Отзыв"
         verbose_name_plural = "Отзывы"
 
     def format_text(self):
-        clean_repeated_puncts = re.sub(r'([.\?#,<>%~`!$:;])\1+', r'\1', self.text)
-        fix_spaces = re.sub(' +', ' ', clean_repeated_puncts)
-        formatted_text = re.sub(r'\s([?.!"](?:\s|$))', r'\1', fix_spaces)
+        clean_repeated_puncts = re.sub(r"([.\?#,<>%~`!$:;])\1+", r"\1", self.text)
+        fix_spaces = re.sub(" +", " ", clean_repeated_puncts)
+        formatted_text = re.sub(r'\s([?.!"](?:\s|$))', r"\1", fix_spaces)
         # capitalize text if finded >6 uppercase chars in a row
-        if re.search(r'([A-ZА-Я]{6})', formatted_text):
-            formatted_text = re.sub(r"(\A\w)|" +    # start of string
-                "(?<!\.\w)([\.?!] )\w|" +           # after a ?/!/. and a space,
-                                                    # but not after an acronym
-                "\w(?:\.\w)|" +                     # start/middle of acronym
-                "(?<=\w\.)\w",                      # end of acronym
+        if re.search(r"([A-ZА-Я]{6})", formatted_text):
+            formatted_text = re.sub(
+                r"(\A\w)|"
+                + "(?<!\.\w)([\.?!] )\w|"       # start of string
+                +                               # after a ?/!/. and a space,
+                                                # but not after an acronym
+                "\w(?:\.\w)|"                   # start/middle of acronym
+                + "(?<=\w\.)\w",                # end of acronym
                 lambda x: x.group().upper(),
-                formatted_text.lower())
+                formatted_text.lower(),
+            )
         return formatted_text
 
     def save(self, *args, **kwargs):
@@ -75,7 +80,7 @@ class Review(models.Model):
 
 
 class Fword(models.Model):
-    word = models.CharField('Слово', max_length=150)
+    word = models.CharField("Слово", max_length=150)
 
     class Meta:
         verbose_name = "Ругательство"
@@ -86,11 +91,15 @@ class Fword(models.Model):
 
 
 class ExceptionWord(models.Model):
-    word = models.CharField('Слово', max_length=150)
+    word = models.CharField("Слово", max_length=150)
 
     class Meta:
         verbose_name = "Слово-исключение"
         verbose_name_plural = "Слова-исключения"
+
+    def save(self, *args, **kwargs):
+        self.word = self.word.lower()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.word
--- reviews/views.py
+++ reviews/views.py
@@ -7,59 +7,63 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 
 from .forms import ReviewForm
-from .models import Doctor, Review, Fword, ExceptionWord
+from .models import Doctor, ExceptionWord, Fword, Review
 
 
 def show_reviews(request):
+    def check_in_exceptions(word, exceptions):
+        return (
+            word.lower().translate(str.maketrans("", "", string.punctuation))
+            in exceptions
+        )
+
+    def make_case_insensibility_pattern(word):
+        return re.compile(re.escape(word), re.IGNORECASE)
+
+    def highlight_f_words(word, exceptions):
+        if re.search(pattern, word) and not check_in_exceptions(word, exceptions):
+            return word.replace(word, f'<span style="color: red;">{word}</span>')
+        return word
+
     if not request.user.is_staff:
         raise Http404
-    review_list = Review.objects.select_related(
-        'author').order_by("dt_created")
+    review_list = Review.objects.select_related("author", "doctor").order_by(
+        "dt_created"
+    )
     f_words = [f.word for f in Fword.objects.all()]
     exceptions = [e.word.lower() for e in ExceptionWord.objects.all()]
     for review in review_list:
         for f_word in f_words:
-            pattern = re.compile(re.escape(f_word), re.IGNORECASE)
+            pattern = make_case_insensibility_pattern(f_word)
             if re.search(pattern, review.formatted_text):
                 formatted_list = (
-                    word.replace(
-                        word, f'<span style="color: red;">{word}</span>')
-                    if re.search(pattern, word)
-                    and not word.lower().translate(
-                        str.maketrans('', '', string.punctuation)) in exceptions
-                    else word
-                    for word in review.formatted_text.split())
-                review.formatted_text = ' '.join(formatted_list)
+                    highlight_f_words(word, exceptions)
+                    for word in review.formatted_text.split()
+                )
+                review.formatted_text = " ".join(formatted_list)
     paginator = Paginator(review_list, 10)
-    page_number = request.GET.get('page')
+    page_number = request.GET.get("page")
     page = paginator.get_page(page_number)
-    return render(
-        request,
-        "index.html",
-        {'page': page, 'paginator': paginator}
-    )
+    return render(request, "index.html", {"page": page, "paginator": paginator})
 
 
 def new_review(request, doctor_id):
     doctor = get_object_or_404(Doctor, id=doctor_id)
-    if request.method == 'POST':
+    if request.method == "POST":
         instance = Review(doctor=doctor)
         if not request.user.is_anonymous:
             instance.author = request.user
-        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
         if x_forwarded_for:
-            ip = x_forwarded_for.split(',')[0]
+            ip = x_forwarded_for.split(",")[0]
         else:
-            ip = request.META.get('REMOTE_ADDR')
+            ip = request.META.get("REMOTE_ADDR")
         instance.ip_address = ip
-        form = ReviewForm(
-            request.POST, instance=instance)
+        form = ReviewForm(request.POST, instance=instance)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Отзыв успешно отправлен')
-        return render(request, 'new_review.html', {
-            'doctor': doctor,
-            'form': form, })
+            messages.success(request, "Отзыв успешно отправлен")
+        return render(request, "new_review.html", {"doctor": doctor, "form": form,})
 
     form = ReviewForm()
-    return render(request, 'new_review.html', {'doctor': doctor, 'form': form, })
+    return render(request, "new_review.html", {"doctor": doctor, "form": form,})
